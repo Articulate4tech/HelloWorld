e04aa82d4bd5a569b376d9f099a4ff49e97a6728
Ayobami Ogunniyi <ogunniyima@gmail.com>
2024-09-13 07:40:52 -0700

    i do

--- addition.js
+++ addition.js
@@ -3,7 +3,7 @@ function add(a, b) {
     return a + b;
     }
     
-    let result = add(5, 3);
+    let result = add(2, 3);
     console.log(result);
 
     //This is to test that code is commiting .
